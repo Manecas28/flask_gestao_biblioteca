276277e07152014b5a0dc479be2ab89a3dd191ea
manuelpintogomes <manuelpintogomes@hotmail.com>
2025-05-09 15:17:54 +0100

    Criação da rota registo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 from models import db, Livro, Utilizador
-from forms import LivroForm
+from forms import LivroForm, LoginForm, RegistoForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 
@@ -10,6 +10,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.secret_key = 'segredomuitobemguardado'
 db.init_app(app)
+
+# Criação de login
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+
+def load_user(user_id):
+    return Utilizador.query.get(int(user_id))
 
 # Criar a Base de dados
 
@@ -53,15 +63,20 @@ def apagar(id):
     db.session.commit()
     return redirect(url_for('listar'))
 
-# Criação de login
-
-login_manager = LoginManager(app)
-login_manager.login_view = "login"
-
-@login_manager.user_loader
-
-def load_user(user_id):
-    return Utilizador.query.get(int(user_id))
+@app.route('/registo', methods = ['GET', 'POST'])
+def registo():
+    form = RegistoForm()
+    if form.validate_on_submit():
+        if Utilizador.query.filter_by(username = form.username.data).first():
+            return "O utilizador já existe, escolha outro nome de utilizador"
+        
+        novo_utilizador = Utilizador(nome = form.nome.data)
+        novo_utilizador = Utilizador(username = form.username.data)
+        novo_utilizador.set_password(form.password.data)
+        db.session.add(novo_utilizador)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('registo.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
